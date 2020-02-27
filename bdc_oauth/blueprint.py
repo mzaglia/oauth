24ee404bc20df4758d4177a1c0b5573b643b7a28
Beto Noronha <beto_noronha@live.com>
2020-02-27 17:14:44 -0300

    add initial tests - architecture

--- bdc_oauth/blueprint.py
+++ bdc_oauth/blueprint.py
@@ -10,9 +10,9 @@ from flask import Blueprint
 from flask_restplus import Api
 
 from bdc_oauth.auth.controller import api as auth_ns
+from bdc_oauth.clients.controller import api as clients_ns
 from bdc_oauth.status.controller import api as status_ns
 from bdc_oauth.users.controller import api as users_ns
-from bdc_oauth.clients.controller import api as clients_ns
 
 bp = Blueprint('oauth', __name__, url_prefix='/oauth')
 
---
+++ tests/bdc_oauth/test_utils.py
@@ -0,0 +1,27 @@
+#
+# This file is part of OBT OAuth 2.0.
+# Copyright (C) 2019-2020 INPE.
+#
+# OBT OAuth 2.0 is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+import unittest
+from bdc_oauth.utils.helpers import random_string
+
+
+class TestUtils(unittest.TestCase):
+
+    def setUp(self):
+        self.key = random_string(20)
+
+    def test_random_string_size(self):
+        self.assertEqual(40, len(self.key))
+
+    def test_random_string_type(self):
+        self.assertIsInstance(self.key, str)
+
+    def test_random_string_error(self):
+        self.assertNotEqual(22, len(self.key))
+
+    
